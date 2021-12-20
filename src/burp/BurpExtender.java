5356999ddd9d5ccadb967904e1bb4c561417b508
fiord <hyu.ogawa@dena.com>
2021-12-20 18:23:56 +0900

    fix: pyro version etc

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,92 +1,109 @@
 package burp;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+
 import com.google.common.primitives.Bytes;
-import net.razorvine.pyro.*;
+import net.razorvine.pyro.PyroProxy;
+import net.razorvine.pyro.PyroURI;
 
 public class BurpExtender implements IBurpExtender, IHttpListener {
     private PrintWriter stdout;
     private PrintWriter stderr;
-
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers helpers;
 
-    @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+        // Set the name of the extension
         callbacks.setExtensionName("Brida Encrypt Communication Sample Plugin");
-
+        // Initialize stdout and stderr (configurable from the Extension pane)
         stdout = new PrintWriter(callbacks.getStdout(), true);
         stderr = new PrintWriter(callbacks.getStderr(), true);
+        // Save references to useful objects
         this.callbacks = callbacks;
         this.helpers = callbacks.getHelpers();
-
+        // Register ourselves as an HttpListener, in this way all requests and responses
+        // will be forwarded to us
         callbacks.registerHttpListener(this);
     }
 
-    @Override
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
+
+        // Process only Repeater, Scanner and Intruder requests
         if (toolFlag == IBurpExtenderCallbacks.TOOL_SCANNER ||
                 toolFlag == IBurpExtenderCallbacks.TOOL_REPEATER ||
                 toolFlag == IBurpExtenderCallbacks.TOOL_INTRUDER) {
-            // request
+
+            // Modify "test" parameter of Repeater requests
             if (messageIsRequest) {
+                // Get request bytes
                 byte[] request = messageInfo.getRequest();
+                // Get a IRequestInfo object, useful to work with the request
+                // IRequestInfo requestInfo = helpers.analyzeRequest(request);
+                // Get "test" parameter
                 IParameter contentParameter = helpers.getRequestParameter(request, "name");
                 if (contentParameter != null) {
+                    String urlDecodedContentParameterValue = helpers.urlDecode(contentParameter.getValue());
                     String ret = "";
+                    // Ask Brida to encrypt our attack vector
                     String pyroUrl = "PYRO:BridaServicePyro@localhost:9999";
                     try {
                         PyroProxy pp = new PyroProxy(new PyroURI(pyroUrl));
-                        ret = (String) pp.call("callexportfunction",
-                                "dev.fiord.encrypted_communication_sample.data.LoginDataSource.encrypt",
-                                new String[] { contentParameter.getValue() });
+                        ret = (String) pp.call("callexportfunction", "encryptsampleapp",
+                                new String[] { urlDecodedContentParameterValue });
                         pp.close();
-                    } catch (IOException e) {
-                        // EXCEPTION HANDLING
-                        stderr.println(e);
+                    } catch (Exception e) {
+                        stderr.println(e.toString());
                         StackTraceElement[] exceptionElements = e.getStackTrace();
                         for (int i = 0; i < exceptionElements.length; i++) {
                             stderr.println(exceptionElements[i].toString());
                         }
                     }
-
+                    // Create the new parameter
                     IParameter newTestParameter = helpers.buildParameter(contentParameter.getName(),
                             helpers.urlEncode(ret), contentParameter.getType());
+                    // Create the new request with the updated parameter
                     byte[] newRequest = helpers.updateParameter(request, newTestParameter);
+                    // Update the messageInfo object with the modified request (otherwise the
+                    // request remains the old one)
                     messageInfo.setRequest(newRequest);
                 }
-            }
-            // response
-            else {
+
+                // Response
+            } else {
+                // Get request bytes in order to check if the request contain "content"
+                // parameter
                 byte[] request = messageInfo.getRequest();
                 IRequestInfo requestInfo = helpers.analyzeRequest(request);
-                String body = new String(Arrays.copyOfRange(request, requestInfo.getBodyOffset(), request.length),
-                        java.nio.charset.StandardCharsets.UTF_8);
-                if (body != null) {
-                    String ret = "";
-                    String pyroUrl = "PYRO:BridaServicePyro@localhost:9999";
-                    try {
-                        PyroProxy pp = new PyroProxy(new PyroURI(pyroUrl));
-                        ret = (String) pp.call("callexportfunction",
-                                "dev.fiord.encrypted_communication_sample.data.LoginDataSource.decrypt",
-                                new String[] { body });
-                        pp.close();
-                    } catch (IOException e) {
-                        // EXCEPTION HANDLING
-                        stderr.println(e);
-                        StackTraceElement[] exceptionElements = e.getStackTrace();
-                        for (int i = 0; i < exceptionElements.length; i++) {
-                            stderr.println(exceptionElements[i].toString());
-                        }
+                // Get response bytes
+                byte[] response = messageInfo.getResponse();
+                // Get a IResponseInfo object, useful to work with the request
+                IResponseInfo responseInfo = helpers.analyzeResponse(response);
+                // Get the offset of the body
+                int bodyOffset = responseInfo.getBodyOffset();
+                // Get the body (byte array and String)
+                byte[] body = Arrays.copyOfRange(response, bodyOffset, response.length);
+                String bodyString = helpers.bytesToString(body);
+                String ret = "";
+                // Ask Brida to decrypt the response
+                String pyroUrl = "PYRO:BridaServicePyro@localhost:9999";
+                try {
+                    PyroProxy pp = new PyroProxy(new PyroURI(pyroUrl));
+                    ret = (String) pp.call("callexportfunction", "decryptsampleapp", new String[] { bodyString });
+                    pp.close();
+                } catch (Exception e) {
+                    stderr.println(e.toString());
+                    StackTraceElement[] exceptionElements = e.getStackTrace();
+                    for (int i = 0; i < exceptionElements.length; i++) {
+                        stderr.println(exceptionElements[i].toString());
                     }
-
-                    byte[] newRequest = Bytes.concat(
-                            Arrays.copyOfRange(request, 0, requestInfo.getBodyOffset()),
-                            ret.getBytes(java.nio.charset.StandardCharsets.UTF_8));
-                    messageInfo.setRequest(newRequest);
                 }
+                // Update the messageInfo object with the modified request (otherwise the
+                // request remains the old one)
+                byte[] newResponse = Bytes.concat(
+                        Arrays.copyOfRange(request, 0, requestInfo.getBodyOffset()),
+                        ret.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+                messageInfo.setResponse(newResponse);
             }
         }
     }
